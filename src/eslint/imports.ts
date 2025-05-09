835b39330e94eac543c35ee7e038567373b4d1ff
jaskang <jaskang@qq.com>
2025-05-09 11:12:59 +0800

    fix: 调整 import 规则

--- src/eslint/imports.ts
+++ src/eslint/imports.ts
@@ -8,12 +8,9 @@ export default function imports(): ConfigWithExtends {
     },
     rules: {
       'import/first': 'error',
-      'import/no-default-export': 'error',
       'import/no-duplicates': 'error',
       'import/no-mutable-exports': 'error',
-      'import/no-named-default': 'error',
       'import/no-self-import': 'error',
-      'import/no-webpack-loader-syntax': 'error',
     },
   }
 }
